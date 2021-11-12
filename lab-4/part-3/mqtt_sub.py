da4f54affc286a32809fb582b2f591c2a3929c03
Francisco Carpio <carpiofj@gmail.com>
2021-11-12 18:57:25 +0100

    refactoring code from lab 6

--- lab-4/part-3/mqtt_sub.py
+++ lab-4/part-3/mqtt_sub.py
@@ -1,10 +1,5 @@
 import paho.mqtt.client as mqtt  # Paho MQTT client
 
-# parameters
-MQTT_BROKER_IP = "192.168.1.3"
-MQTT_BROKER_PORT = 1883
-MQTT_TOPIC = "lab4"
-
 
 # callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -13,7 +8,7 @@ def on_connect(client, userdata, flags, rc):
 
 # callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload.decode("utf-8")))
+    print("Message received [" + msg.topic + "]: " + str(msg.payload.decode("utf-8")))
 
 
 def on_subscribe(mqttc, obj, mid, granted_qos):
@@ -24,14 +19,23 @@ def on_log(mqttc, obj, level, string):
     print(string)
 
 
-mqttc = mqtt.Client()
-mqttc.on_connect = on_connect
-mqttc.on_message = on_message
-mqttc.on_subscribe = on_subscribe
-# Uncomment to enable debug messages
-# mqttc.on_log = on_log
+def connect_to_mqtt_broker(ip, port):
+    mqttc = mqtt.Client()
+    mqttc.on_connect = on_connect
+    mqttc.on_message = on_message
+    mqttc.on_subscribe = on_subscribe
+    # mqttc.on_log = on_log # Uncomment to enable debug messages
+    mqttc.connect(ip, port, 60)  # connect to the broker
+    return mqttc
 
-mqttc.connect(MQTT_BROKER_IP, MQTT_BROKER_PORT, 60)  # connect to the broker
-mqttc.subscribe(MQTT_TOPIC)  # subscribe to the topic
 
-mqttc.loop_forever()
+if __name__ == "__main__":
+    # parameters
+    mqtt_broker_ip = "192.168.1.3"
+    mqtt_broker_port = 1883
+    mqtt_topic = "lab4"
+
+    mqttc = connect_to_mqtt_broker(mqtt_broker_ip, mqtt_broker_port)
+    mqttc.subscribe(mqtt_topic)  # subscribe to the topic
+
+    mqttc.loop_forever()
