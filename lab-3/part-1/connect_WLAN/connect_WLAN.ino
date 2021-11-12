fbd39ea8361a0b40d2c16da53ab090a2e801f4d9
Francisco Carpio <carpiofj@gmail.com>
2021-11-12 11:28:03 +0100

    refactoring lab-3

--- lab-3/part-1/connect_WLAN/connect_WLAN.ino
+++ lab-3/part-1/connect_WLAN/connect_WLAN.ino
@@ -1,23 +1,14 @@
-#include <ESP8266WiFi.h> // wifi module library 
+#include "connect_WLAN.h"
 
-String ssid = "netw1"; // student's network SSID
-String pass = "password1"; //student's network password 
+String ssid = "netw1";     // student's network SSID
+String pass = "password1"; //student's network password
 
-void setup() {
-  Serial.begin(115200); // establish serial communication at baud rate 115200
-  ConnectoToWiFi(); // connect to the network
-}
- 
-void ConnectoToWiFi() {
-  Serial.println("Trying to connect to " + ssid);
-  WiFi.begin(ssid,pass); // connect to WiFi
-  while (WiFi.status() != WL_CONNECTED) { 
-    delay(500); // add delay until network connected 
-    Serial.print(".");
-  }
-  Serial.print("Connected to WiFi, IP address: ");
-  Serial.println(WiFi.localIP());
+void setup()
+{
+  Serial.begin(115200);        // establish serial communication at baud rate 115200 || 9200 (vscode)
+  connect_to_wifi(ssid, pass); // connect to the network
 }
 
-void loop() {
+void loop()
+{
 }
