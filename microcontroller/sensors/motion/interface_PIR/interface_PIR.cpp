4e2937ca66bfb7c9a3146c6981d0321680d690d1
Jasenka Dizdarevic <j.dizdarevic@tu-bs.de>
2022-03-11 18:32:13 +0100

    PIR sensor code updated

--- microcontroller/sensors/motion/interface_PIR/interface_PIR.cpp
+++ microcontroller/sensors/motion/interface_PIR/interface_PIR.cpp
@@ -6,14 +6,6 @@ const int pirSensor = 13; // PIR's pin is connected to NodeMCU's GPIO13 (D7) or 
 void initialize_sensor()
 {
   pinMode(pirSensor, INPUT); // set sensor pin as input:
-  Serial.print("Calibrating sensor... ");
-  int calibrationTime = 30;                 //time for sensor to calibrate
-  for (int i = 0; i < calibrationTime; i++) // give some time to calibrate
-  {
-    Serial.print(".");
-    delay(100);
-  }
-  Serial.println("Sensor calibrated");
 }
 
 bool detect_motion()
