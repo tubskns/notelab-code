fb3a653940cea8327980daa15167f106ab45f67c
Francisco Carpio <carpiofj@gmail.com>
2022-03-18 16:15:07 +0100

    reduces vertical spacing in mqtt_client

--- rpi/mqtt_client.py
+++ rpi/mqtt_client.py
@@ -1,18 +1,16 @@
-import paho.mqtt.client as mqtt  # Paho MQTT client
+import paho.mqtt.client as mqtt
 
 
-# callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("MQTT client - connected to broker with result code " + str(rc))
+    print("MQTT client - connected to broker, code " + str(rc))
 
 
-# callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     global message
     message = msg
     topic = msg.topic
     payload = str(msg.payload.decode("utf-8"))
-    print("MQTT client - message received [" + topic + "]: " + payload)
+    print("MQTT client - msg received [" + topic + "]: " + payload)
 
 
 def on_subscribe(mqttc, obj, result, granted_qos):
@@ -33,9 +31,9 @@ def connect(ip, port=1883):
     mqttc.on_message = on_message
     mqttc.on_publish = on_publish
     mqttc.on_subscribe = on_subscribe
-    # mqttc.on_log = on_log # Uncomment to enable debug messages
+    mqttc.on_log = on_log
     try:
-        mqttc.connect(ip, port, 60)  # connect to the broker
+        mqttc.connect(ip, port, 60) 
     except:
         print("MQTT client - [" + ip + ":" + str(port) + "] connection failed!, exiting...")
         exit(1)
@@ -43,12 +41,9 @@ def connect(ip, port=1883):
 
 
 if __name__ == "__main__":
-    # parameters
     mqtt_broker_ip = "broker.hivemq.com"
     mqtt_broker_port = 1883
     mqtt_topic = "testMQTT"
-
     mqttc = connect(mqtt_broker_ip, mqtt_broker_port)
-    mqttc.subscribe(mqtt_topic)  # subscribe to the topic
-
+    mqttc.subscribe(mqtt_topic)
     mqttc.loop_forever()
