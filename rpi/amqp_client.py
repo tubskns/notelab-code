887b2aca4e0162922b82881522b27a9f39ef9ac2
Francisco Carpio <carpiofj@gmail.com>
2022-04-04 15:52:02 +0200

    updating amqp client

--- rpi/amqp_client.py
+++ rpi/amqp_client.py
@@ -1,42 +1,41 @@
 import pika
 
-
-def publish(channel, msg, exchange="amq.topic", routing_key="logstash"):
-    channel.basic_publish(exchange=exchange, routing_key=routing_key, body=msg)
-    print("AMQP publisher - Message sent: " + msg)
-
-
-def connect_to_broker(ip, port=5672, user="user", passw="password", queue="notelab"):
+def connect_to_broker(ip, port, user, passw, queue):
     credentials = pika.PlainCredentials(user, passw)
     parameters = pika.ConnectionParameters(ip, port, "/", credentials)
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
-    channel.queue_declare(queue=queue, durable=True)  # queue to send data
     return channel
+
+
+def publish(channel, msg, exchange, routing_key):
+    channel.queue_declare(queue=queue, durable=True)
+    channel.queue_bind(exchange=exchange, queue=queue)
+    channel.basic_publish(exchange=exchange, routing_key=routing_key, body=msg)
+    print("AMQP publisher - Message sent: " + msg)
+
 
 def on_message(channel, method, properties, msg):
     global message
     message = msg
-    print("AMQP consumer - Message receieved: " + msg.decode("utf-8"))
+    print("AMQP consumer - Message received: " + msg.decode("utf-8"))
+
 
 def subscribe(channel, queue):
     channel.basic_consume(queue, on_message_callback=on_message, auto_ack=True)
-    channel.start_consuming()
     print("AMQP subscriber - Subscribed to: " + queue)
+    channel.start_consuming()
+
 
 if __name__ == "__main__":
-    #  server parameters - RabbitMq
-    ip = "192.168.1.7"  # server IP address
-    port = 5672  #
+    ip = "192.168.1.7"
+    port = 5672
     user = "user"
     passw = "password"
     queue = "notelab"
     exchange = "amq.topic"
-    routing_key = "test"
+    routing_key = queue
     message = "test_amqp_message"
     channel = connect_to_broker(ip, port, user, passw, queue)
     publish(channel, message, exchange, routing_key)
-    print("Message sent to RabbitMQ: " + message)
     subscribe(channel, queue)
-    print("Message received from RabbitMQ: " + message)
-  
