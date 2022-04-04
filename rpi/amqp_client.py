89443b7ac0a905ebef524a981e4bb911968b58b6
Jasenka Dizdarevic <j.dizdarevic@tu-bs.de>
2022-04-04 16:09:46 +0200

    modifications in queue exchnage binding

--- rpi/amqp_client.py
+++ rpi/amqp_client.py
@@ -1,6 +1,6 @@
 import pika
 
-def connect_to_broker(ip, port, user, passw, queue):
+def connect_to_broker(ip, port, user, passw):
     credentials = pika.PlainCredentials(user, passw)
     parameters = pika.ConnectionParameters(ip, port, "/", credentials)
     connection = pika.BlockingConnection(parameters)
@@ -8,9 +8,9 @@ def connect_to_broker(ip, port, user, passw, queue):
     return channel
 
 
-def publish(channel, msg, exchange, routing_key):
+def publish(channel, msg, exchange, routing_key, queue):
     channel.queue_declare(queue=queue, durable=True)
-    channel.queue_bind(exchange=exchange, queue=queue)
+    channel.queue_bind(exchange=exchange, queue=queue, routing_key=routing_key)
     channel.basic_publish(exchange=exchange, routing_key=routing_key, body=msg)
     print("AMQP publisher - Message sent: " + msg)
 
@@ -32,10 +32,10 @@ if __name__ == "__main__":
     port = 5672
     user = "user"
     passw = "password"
-    queue = "notelab"
+    queue = "queue_test"
     exchange = "amq.topic"
-    routing_key = queue
-    message = "test_amqp_message"
-    channel = connect_to_broker(ip, port, user, passw, queue)
-    publish(channel, message, exchange, routing_key)
+    routing_key = "routing_key_test"
+    message = "message_test"
+    channel = connect_to_broker(ip, port, user, passw)
+    publish(channel, message, exchange, routing_key, queue)
     subscribe(channel, queue)
