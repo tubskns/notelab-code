9a86c5574de7e264b50df214ba744983098dc6e4
Francisco Carpio <carpiofj@gmail.com>
2022-04-08 17:07:26 +0200

    updating code for DHT11 sensor

--- microcontroller/sensors/temperature/DHT11/DHT11.ino
+++ microcontroller/sensors/temperature/DHT11/DHT11.ino
@@ -1,12 +1,10 @@
-#include "DHT11.h"
 #include "DHT.h" // DHT module library
 
-#define DHTTYPE DHT_TYPE // DHT type
 #define DHTPIN 0 // DHT11's data pin is connected to NodeMCU's GPIO0 (D3)
 // #define DHTPIN D5 // DHT11's data pin is connected to WeMos GPIO14 (D5)
 
-DHT dht(DHTPIN, DHT_TYPE); // instantiate DHT object
-DHT11 dht11(dht);
+#define DHTTYPE DHT11 // DHT type
+DHT dht(DHTPIN, DHT11); // instantiate DHT object
 
 void setup()
 {
@@ -15,13 +13,21 @@ void setup()
 
 void loop()
 {
-  float humidity = dht11.get_humidity();
-  String humidity_str = String(humidity); //convert float to string
-  Serial.print("Humidity: " + humidity_str + " %\n");
+  float humidity = dht.readHumidity(); 
+  if (isnan(humidity)) {
+    String humidity_str = String(humidity); //convert float to string
+    Serial.print("Humidity: " + humidity_str + " %\n");
+  } else {
+    Serial.println("Humidity reading from DHT11 sensor failed!");
+  }
 
-  float temperature = dht11.get_temperature();
-  String temperature_str = String(temperature); //convert float to string
-  Serial.print("Temperature: " + temperature_str + " *C\n");
-
+  float temperature = dht.readTemperature(); 
+  if (isnan(temperature))
+  {
+    String temperature_str = String(temperature); //convert float to string
+    Serial.print("Temperature: " + temperature_str + " *C\n");
+  } else {
+    Serial.println("Temperature reading from DHT11 sensor failed!");
+  }
   delay(1000);
 }
