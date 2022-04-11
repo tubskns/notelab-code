37fa561a6904a38170588ffc36434f2db19900fc
Francisco Carpio <carpiofj@gmail.com>
2022-04-11 13:19:01 +0200

    reducing number of lines in connector_2

--- rpi/connector_2.py
+++ rpi/connector_2.py
@@ -1,42 +1,17 @@
-import mqtt_client, amqp_client, http_client
-import time
-import json
-
-# elastic
-url_elastic = "http://192.168.1.7:9200"
-index_elastic = "notelab"
-# rabbit
-ip_rabbit = "134.169.115.45"
-exchange_rabbit = "amq.topic"
-r_key_rabbit = "notelab"
-queue_rabbit = "notelab_queue"
-# mosquitto
-ip_mosquitto = "192.168.1.3"
-actuator1_topic_mosquitto = "test_topic_1"
-actuator2_topic_mosquitto = "test_topic_2"
-
-# connect to mosquitto
-mqttc = mqtt_client.connect(ip_mosquitto)
+import mqtt_client, amqp_client, http_client, json
 
 
 def on_amqp_message(channel, method, properties, msg):
-    # rabbit
     print("Connector_2 - AMQP consumer - Message received: " + msg.decode("utf-8"))
     payload_rabbit = str(msg.decode("utf-8"))
-    # elastic
     print("Connector_2 - HTTP GET - Retrieving message from Elasticsearch...")
-    res_elastic = http_client.query(
-        url_elastic + "/" + index_elastic + "/_search",
-        method="GET",
-    )
+    res_elastic = http_client.query(url_elastic + "/" + index_elastic + "/_search",method="GET")
     payload_elastic = None
     if res_elastic is not None:
-        if res_elastic["hits"]["total"]["value"] > 0: 
+        if res_elastic["hits"]["total"]["value"] > 0:
             hits = res_elastic["hits"]["hits"]
             last_hit = hits[len(hits) - 1]
-            msg = last_hit["_source"]
-            payload_elastic = json.dumps(msg)
-    # mosquitto
+            payload_elastic = json.dumps(last_hit["_source"])
     if payload_elastic is not None:
         print("Connector_2 - MQTT publisher - Forwarding data from Elasticsearch to Mosquitto: " + payload_elastic)
         mqttc.publish(actuator1_topic_mosquitto, payload_elastic)
@@ -45,9 +20,18 @@ def on_amqp_message(channel, method, properties, msg):
         mqttc.publish(actuator2_topic_mosquitto, payload_rabbit)
 
 
-# connect to rabbitmq and subscribe to topics
+url_elastic = "http://192.168.1.7:9200"
+index_elastic = "notelab"
+ip_rabbit = "192.168.1.3"
+exchange_rabbit = "amq.topic"
+r_key_rabbit = "notelab"
+queue_rabbit = "notelab_queue"
+ip_mosquitto = "192.168.1.3"
+actuator1_topic_mosquitto = "test_topic_1"
+actuator2_topic_mosquitto = "test_topic_2"
+
+mqttc = mqtt_client.connect(ip_mosquitto) 
+
 amqp_client.message = None
-amqp_channel = amqp_client.connect_to_broker(
-    ip_rabbit, port=5672, user="user", passw="password"
-)
+amqp_channel = amqp_client.connect_to_broker(ip_rabbit, port=5672, user="user", passw="password") 
 amqp_client.subscribe(amqp_channel, queue_rabbit, on_amqp_message)
