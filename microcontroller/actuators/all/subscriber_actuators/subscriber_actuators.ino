8238678b0a36029b75d27106197059e599ce3a47
Francisco Carpio <carpiofj@gmail.com>
2022-04-12 14:28:41 +0200

    updating subscriber_actuators

--- microcontroller/actuators/all/subscriber_actuators/subscriber_actuators.ino
+++ microcontroller/actuators/all/subscriber_actuators/subscriber_actuators.ino
@@ -7,68 +7,53 @@ String pass_wifi = "password1";
 const char *mqtt_broker_ip = "192.168.1.3";
 const int mqtt_broker_port = 1883;
 const char *client_id = "subscriber_sensors";
-char *temp_topic = "temperature_topic";
-char *dist_topic = "distance_topic";
-char *motion_topic = "motion_topic";
-#define LED1 D1 // green LED connected to GPIO5 (D1)
-#define LED2 D2 // yellow LED is connected to GPIO4 (D2)
-#define LED3 D3 // red LED is connected to GPIO (D3)
+char *topics = {"temp_topic", "dist_topic", "motion_topic"}
+uint8_t *leds = {D1, D2, D3} // green, yellow, red
 DynamicJsonDocument msg_doc(1024);
 float prev_temp = 0;
+int led2_state = LOW; 
+long led2_interval = 10000000;
+unsigned long prev_millis = 0;
 
-void setup()
-{
+void setup(){
   Serial.begin(115200);
   connect_to_wifi(ssid_wifi, pass_wifi);
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
+  for (int i = 0; i < (sizeof(leds) / sizeof(leds[0])); i++) {
+    pinMode(leds[i], OUTPUT);
+  }
   initialize_client(mqtt_broker_ip, mqtt_broker_port);
   connect(client_id);
-  subscribe_to_topic(temp_topic);
-  subscribe_to_topic(dist_topic);
-  subscribe_to_topic(motion_topic);
+  subscribe_to_topic(topics);
 }
 
 void loop(){
   check_connection(client_id);
   String msg = get_msg();
   String topic = get_topic();
+  unsigned long current_millis = millis();
   deserializeJson(msg_doc, msg);
-  if (topic == motion_topic){
+  if (topic == topics[0]){
     bool motion = msg_doc["motion"];
-    if (motion == true){
-      digitalWrite(LED1, HIGH);
-    }
-    else{
-      digitalWrite(LED1, LOW);
-    }
+    if (motion == true){ digitalWrite(leds[0], HIGH); }
+    else{ digitalWrite(leds[0], LOW); }
   }
-  else if (topic == dist_topic){
+  else if (topic == topics[1]){
+    if (current_millis - prev_millis >= led2_interval) {
+      prev_millis = current_millis;
+      if (led2_state == LOW) { led2_state = HIGH; } 
+      else { led2_state = LOW; }
+      digitalWrite(leds[1], led2_state);
+    }
     float distance = msg_doc["distance"];
-    float blink_delay=0;
-    if (distance >= 1.0 && distance < 5.0){
-      blink_delay = 500;
-    }
-    else if (distance >= 5.0 && distance < 10.0){
-      blink_delay = 1000;
-    }
-    else{
-      blink_delay = 2000;
-    }
-    digitalWrite(LED2, HIGH);
-    delay(blink_delay);
-    digitalWrite(LED2, LOW);
-    delay(blink_delay);
+    if (distance <= 5.0){ led2_interval = 500; }
+    else { led2_interval = 2000; }
   }
-  else if (topic == temp_topic){
+  else if (topic == topics[2]){
     float temperature = msg_doc["temperature"];
     if (temperature != prev_temp){
-      digitalWrite(LED3, HIGH);
+      digitalWrite(leds[2], HIGH);
       prev_temp = temperature;
-    } else {
-      digitalWrite(LED3, LOW);
-    }
-  }
-  else{Serial.println("Topic not matching, ignoring...");}
+    } else { digitalWrite(leds[2], LOW); }
+  } else{Serial.println("Topic not matching, ignoring...");}
+  reset_msg();
 }
--- microcontroller/mqtt_client/mqtt_client.cpp
+++ microcontroller/mqtt_client/mqtt_client.cpp
@@ -9,8 +9,7 @@ String _topic;
 
 void connect(const char *client_id){
   Serial.print("\nConnecting to MQTT broker... ");
-  while (!mqtt_client.connect(client_id, "public", "public"))
-  {
+  while (!mqtt_client.connect(client_id, "public", "public")){
     Serial.print(".");
     delay(1000);
   }
@@ -27,10 +26,12 @@ void initialize_client(const char *mqtt_broker_ip, const int mqtt_broker_port){
   mqtt_client.begin(mqtt_broker_ip, wifi_client);
 }
 
-void subscribe_to_topic(char *topic){
-  Serial.println("\nSubscribing to topic: " + String(topic));
-  mqtt_client.onMessage(callback);
-  mqtt_client.subscribe(topic);
+void subscribe_to_topic(char *topics){
+  for (int i = 0; i < (sizeof(topic) / sizeof(topic[0])); i++) {
+      Serial.println("Subscribing to topic: " + String(topics[i]));
+      mqtt_client.onMessage(callback);
+      mqtt_client.subscribe(topics[i]);
+  }
 }
 
 void publish_message(char *topic, char *msg){
@@ -44,10 +45,11 @@ void check_connection(const char *client_id){
   if (!mqtt_client.connected()){ connect(client_id); }
 }
 
-String get_msg(){
-  return _msg;
-}
+String get_msg(){ return _msg; }
 
-String get_topic(){
-  return _topic;
+String get_topic(){ return _topic; }
+
+void reset_msg(){
+  _msg = ""
+  _topic = ""
 }
