dae2e5fa65cebbdcb0ee79b65371f53a255d9bbb
Jasenka Dizdarevic <j.dizdarevic@tu-bs.de>
2022-05-13 18:33:58 +0200

    update  publisher for all sensors

--- microcontroller/sensors/all/publisher_sensors/publisher_sensors.ino
+++ microcontroller/sensors/all/publisher_sensors/publisher_sensors.ino
@@ -1,36 +1,39 @@
-#include "mqtt_client.h"
-#include "wifi_connection.h"
+#include "MqttClient.h"
+#include "WifiClient.h"
 #include "HCSR04.h"
 #include "PIR.h"
 #include "DHT.h"
 #include <ArduinoJson.h>
 
-String ssid_wifi = "netw0"; 
+String ssid_wifi = "netw0";     
 String pass_wifi = "password1";
-const char* mqtt_broker_ip = "192.168.1.3";
+
+const char *mqtt_broker_ip = "192.168.1.3";
 const int mqtt_broker_port = 1883;
 const char* client_id = "publisher_sensors";
 char* temp_topic = "temp_topic";
 char* dist_topic = "dist_topic";
 char* motion_topic = "motion_topic";
+WifiClient wifi_client(ssid_wifi, pass_wifi);
+MqttClient mqtt_client(mqtt_broker_ip, mqtt_broker_port);
 
 #define DHTPIN 0 // DHT11's pin
 #define DHTTYPE DHT11 // DHT11's type
 DHT dht(DHTPIN, DHT11);
 HCSR04 hcsr04(5, 4); // specify HC-SR04 trig and echo pins
 PIR pir(13); // specify PIR's pin
+
 DynamicJsonDocument temp_doc(1024), dist_doc(1024), motion_doc(1024);
-String *no_sub_topics;
 
 void setup(){
   Serial.begin(115200);
-  connect_to_wifi(ssid_wifi, pass_wifi);
-  initialize_client(mqtt_broker_ip, mqtt_broker_port);
-  dht.begin();
+  wifi_client.connect();
+  mqtt_client.connect(client_id);
+  dht.begin(); // initialize the sensor
 }
 
 void loop(){
-  check_connection(client_id, no_sub_topics, 0);
+  mqtt_client.check_connection(client_id);
   float temperature = dht.readTemperature(); 
   if (!isnan(temperature)){
     temp_doc["id"] = client_id;
@@ -44,7 +47,7 @@ void loop(){
   serializeJson(temp_doc, temp_out);
   serializeJson(dist_doc, dist_out);
   serializeJson(motion_doc, motion_out);
-  publish_message(temp_topic, temp_out);
-  publish_message(dist_topic, dist_out);
-  publish_message(motion_topic, motion_out);
+  mqtt_client.publish_message(temp_topic, temp_out);
+  mqtt_client.publish_message(dist_topic, dist_out);
+  mqtt_client.publish_message(motion_topic, motion_out);
 }
