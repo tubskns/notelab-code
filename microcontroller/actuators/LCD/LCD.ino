4dec26db89931513abaa30388ae934aa0f6ed109
Jasenka Dizdarevic <jasenka.d11@gmail.com>
2022-06-09 17:46:25 +0200

    small mod of LCD code

--- microcontroller/actuators/LCD/LCD.ino
+++ microcontroller/actuators/LCD/LCD.ino
@@ -1,7 +1,7 @@
 #include <LiquidCrystal.h>
 
-byte lcd_cols = 16; // -- number of columns in the LCD
-byte lcd_line = 2; // -- number of line in the LCD
+byte lcd_cols = 16; // number of columns in the LCD
+byte lcd_line = 2; // number of line in the LCD
 int lcd_contrast = 100;
 int lcd_pin = 6;
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
@@ -9,13 +9,12 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 void setup()
 {
    analogWrite(lcd_pin, lcd_contrast);
-   lcd.begin(lcd_cols, lcd_line);
+   lcd.begin(lcd_cols, lcd_line); // set up the LCD's number of columns and rows:
    delay(100);
    lcd.setCursor(0, 0); // first row
-   lcd.print("Temp. +24");
-   lcd.print((char) 223); // celsius char
+   lcd.print("Test message");
    lcd.setCursor(0, 1); // second row
-   lcd.print("Dist. 5 cm");
+   lcd.print("from NoteLab");
 }
 void loop()
 {
