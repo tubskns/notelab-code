b9c88316a929b91b5524264b6a4acd7c4f150f20
Francisco Carpio <carpiofj@gmail.com>
2022-06-20 11:54:35 +0200

    update python examples

--- rpi/example_main.py
+++ rpi/example_main.py
@@ -1,21 +1,16 @@
 import example_module as em
 
+
 def check_sign(value):
     if value > 0:
-        print("Calculated value", value, "is positive")
+        print("Value: ", value, "is positive")
     elif value == 0:
-        print("Calculated value", value, "is equal to zero")
+        print("Value:", value, "is equal to zero")
     else:
-        print("Calculated value", value, "is negative")
+        print("Value:", value, "is negative")
 
-###### first task
+
 check_sign(-5)
 
-###### second task
 value = em.calc_sum([2, -5, 8, -13])
 check_sign(value)
-
-
-
-
-    
