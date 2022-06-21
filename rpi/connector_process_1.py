2dc9967ecc415b1001258f2223928b6578b34f3a
Francisco Carpio <carpiofj@gmail.com>
2022-06-21 19:16:53 +0200

    updating connector_process 1 and 2

--- rpi/connector_process_1.py
+++ rpi/connector_process_1.py
@@ -3,9 +3,9 @@ import amqp_client
 import json, time
 import logging
 
-# rabbit
+
 ip_rabbit = "192.168.1.7"
-exchange_rabbit = "amq.topic"
+ex_rabbit = "amq.topic"
 rkey_rabbit_temp = "temperature"
 queue_rabbit_temp = "temperature"
 rkey_rabbit_dist = "dist_rabbit"
@@ -13,7 +13,6 @@ queue_rabbit_dist = "dist_rabbit"
 rkey_rabbit_motion = "motion_rabbit"
 queue_rabbit_motion = "motion_rabbit"
 
-# mosquitto
 ip_mosquitto = "192.168.1.3"
 topic_temp = "temp_topic"
 topic_dist = "dist_topic"
@@ -24,32 +23,17 @@ topics_mosquitto = [
     (topic_motion, 1),
 ]
 
-# global vars
 IS_ACTIVATED = False
 activation_interval = 5
 start_time = 100000000
 temp_values = []
-logging.basicConfig(
-    format="%(asctime)s | %(name)s | %(levelname)s | %(message)s", level=logging.INFO
-)
+logging.basicConfig(format="%(asctime)s | %(name)s | %(levelname)s | %(message)s", level=logging.INFO)
 
-logging.info("Running connector... ")
+amqp_ch = amqp_client.connect_to_broker(ip_rabbit, port=5672, user="user", passw="password")
+amqp_client.create_queue(amqp_ch, ex_rabbit, rkey_rabbit_temp, queue_rabbit_temp)
+amqp_client.create_queue(amqp_ch, ex_rabbit, rkey_rabbit_dist, queue_rabbit_dist)
+amqp_client.create_queue(amqp_ch, ex_rabbit, rkey_rabbit_motion, queue_rabbit_motion)
 
-# connect to rabbitmq
-amqp_channel = amqp_client.connect_to_broker(
-    ip_rabbit, port=5672, user="user", passw="password"
-)
-amqp_client.create_queue(
-    amqp_channel, exchange_rabbit, rkey_rabbit_temp, queue_rabbit_temp
-)
-amqp_client.create_queue(
-    amqp_channel, exchange_rabbit, rkey_rabbit_dist, queue_rabbit_dist
-)
-amqp_client.create_queue(
-    amqp_channel, exchange_rabbit, rkey_rabbit_motion, queue_rabbit_motion
-)
-
-# connect to mosquitto and subscribe to topics
 mqtt_client.message = None
 mqttc = mqtt_client.connect(ip_mosquitto)
 mqttc.subscribe(topics_mosquitto)
@@ -58,57 +42,34 @@ while True:
     if mqtt_client.message is not None:
         topic = mqtt_client.message.topic
         msg = json.loads(str(mqtt_client.message.payload.decode("utf-8")))
-        if topic == topic_motion:  # check to activate workflow based on motion
-            if msg["motion"]:
-                if not IS_ACTIVATED:  # activate circuit
-                    IS_ACTIVATED = True
-                    start_time = time.time()
-                    amqp_channel.basic_publish(
-                        exchange=exchange_rabbit,
-                        routing_key=rkey_rabbit_motion,
-                        body=json.dumps(msg),
-                    )
-                else:
-                    elapsed_time = time.time() - start_time
-                    if elapsed_time > activation_interval:  # deactivate circuit
-                        amqp_channel.basic_publish(
-                            exchange=exchange_rabbit,
-                            routing_key=rkey_rabbit_motion,
-                            body=json.dumps(msg),
-                        )
-                        IS_ACTIVATED = False
-            else:
-                if not IS_ACTIVATED:
-                    amqp_channel.basic_publish(
-                        exchange=exchange_rabbit,
-                        routing_key=rkey_rabbit_motion,
-                        body=json.dumps(msg),
-                    )
-        elif topic == topic_temp:  # calculate avg temp of x values and send
+        if IS_ACTIVATED:
+            elapsed_time = time.time() - start_time
+            if elapsed_time > activation_interval:
+                logging.debug("Activation interval is over")
+                IS_ACTIVATED = False
+                logging.debug("STOPPING collection of distance...")
+                msg["motion"] = False
+                amqp_ch.basic_publish(ex_rabbit, rkey_rabbit_motion, json.dumps(msg))
+                logging.debug("RESETTING distance to 0... ")
+                msg["distance"] = 0.0
+                amqp_ch.basic_publish(ex_rabbit, rkey_rabbit_dist, json.dumps(msg))
+        if topic == topic_motion:
+            if msg["motion"] and IS_ACTIVATED == False:
+                logging.debug("STARTING collection of distance...")
+                IS_ACTIVATED = True
+                start_time = time.time()
+                amqp_ch.basic_publish(ex_rabbit, rkey_rabbit_motion, json.dumps(msg))
+        elif topic == topic_temp:
             if len(temp_values) < 24:
                 temp_values.append(msg["temperature"])
             else:
+                logging.debug("Sending average temperature... ")
                 avg_temp = sum(temp_values) / len(temp_values)
                 msg["temperature"] = avg_temp
-                amqp_channel.basic_publish(
-                    exchange=exchange_rabbit,
-                    routing_key=rkey_rabbit_temp,
-                    body=json.dumps(msg),
-                )
+                amqp_ch.basic_publish(ex_rabbit, rkey_rabbit_temp, json.dumps(msg))
                 temp_values = []
         elif topic == topic_dist:
             if IS_ACTIVATED:
-                amqp_channel.basic_publish(
-                    exchange=exchange_rabbit,
-                    routing_key=rkey_rabbit_dist,
-                    body=json.dumps(msg),
-                )
-            else:
-                msg["distance"] = 0.0
-                amqp_channel.basic_publish(
-                    exchange=exchange_rabbit,
-                    routing_key=rkey_rabbit_dist,
-                    body=json.dumps(msg),
-                )
-
-        mqtt_client.message = None  # reset message
+                logging.debug("SENDING distance... ")
+                amqp_ch.basic_publish(ex_rabbit, rkey_rabbit_dist, json.dumps(msg))
+        mqtt_client.message = None
